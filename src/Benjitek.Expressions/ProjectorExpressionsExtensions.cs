eff37e0c3320c64ff98dc0337ad9f5cb24d0cf92
Benjamin <benjismit@live.nl>
2025-02-08 17:09:53 +0100

    Add projector expressions extensions

---
+++ src/Benjitek.Expressions/ProjectorExpressionsExtensions.cs
@@ -0,0 +1,170 @@
+﻿using System.Linq.Expressions;
+
+namespace Benjitek.Expressions;
+
+public static class ProjectorExpressionsExtensions
+{
+    /// <summary>
+    ///     Returns a new expression that projects the result of the given expression.
+    /// </summary>
+    /// <typeparam name="TParam">
+    ///     The type of the parameter of the expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult">
+    ///     The type of the result of the expression.
+    /// </typeparam>
+    /// <typeparam name="TProjectionResult">
+    ///     The type of the result of the projection expression.
+    /// </typeparam>
+    /// <param name="expression">
+    ///     The expression to project the result of.
+    /// </param>
+    /// <param name="projector">
+    ///     The expression to project the result of the expression.
+    /// </param>
+    /// <returns>
+    ///     The new expression that projects the result of the given expression.
+    /// </returns>
+    public static Expression<Func<TParam, TProjectionResult>> Project<TParam, TExpressionResult, TProjectionResult>(
+        this Expression<Func<TParam, TExpressionResult>> expression,
+        Expression<Func<TExpressionResult, TProjectionResult>> projector)
+    {
+        var parameter = Expression.Parameter(typeof(TParam), "param");
+
+        var arg = Expression.Invoke(expression, parameter);
+
+        var projection = Expression.Invoke(projector, arg);
+
+        return Expression.Lambda<Func<TParam, TProjectionResult>>(projection, parameter);
+    }
+
+    /// <summary>
+    ///     Returns a new expression that projects the result of the given expressions.
+    /// </summary>
+    /// <typeparam name="TParam">
+    ///     The type of the parameter of the expressions.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult1">
+    ///     The type of the result of the first expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult2">
+    ///     The type of the result of the second expression.
+    /// </typeparam>
+    /// <typeparam name="TProjectionResult">
+    ///     The type of the result of the projection expression.
+    /// </typeparam>
+    /// <param name="expressions">
+    ///     The expressions to project the result of.
+    /// </param>
+    /// <param name="projector">
+    ///     The expression to project the result of the expressions.
+    /// </param>
+    /// <returns>
+    ///     The new expression that projects the result of the given expressions.
+    /// </returns>
+    public static Expression<Func<TParam, TProjectionResult>> Project<TParam, TExpressionResult1, TExpressionResult2, TProjectionResult>(
+        this (Expression<Func<TParam, TExpressionResult1>>, Expression<Func<TParam, TExpressionResult2>>) expressions,
+        Expression<Func<TExpressionResult1, TExpressionResult2, TProjectionResult>> projector
+        )
+    {
+        var parameter = Expression.Parameter(typeof(TParam), "param");
+
+        var arg1 = Expression.Invoke(expressions.Item1, parameter);
+        var arg2 = Expression.Invoke(expressions.Item2, parameter);
+
+        var projection = Expression.Invoke(projector, arg1, arg2);
+
+        return Expression.Lambda<Func<TParam, TProjectionResult>>(projection, parameter);
+    }
+
+    /// <summary>
+    ///     Returns a new expression that projects the result of the given expressions.
+    /// </summary>
+    /// <typeparam name="TParam">
+    ///     The type of the parameter of the expressions.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult1">
+    ///     The type of the result of the first expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult2">
+    ///     The type of the result of the second expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult3">
+    ///     The type of the result of the third expression.
+    /// </typeparam>
+    /// <typeparam name="TProjectionResult">
+    ///     The type of the result of the projection expression.
+    /// </typeparam>
+    /// <param name="expressions">
+    ///     The expressions to project the result of.
+    /// </param>
+    /// <param name="projector">
+    ///     The expression to project the result of the expressions.
+    /// </param>
+    /// <returns>
+    ///     The new expression that projects the result of the given expressions.
+    /// </returns>
+    public static Expression<Func<TParam, TProjectionResult>> Project<TParam, TExpressionResult1, TExpressionResult2, TExpressionResult3, TProjectionResult>(
+        this (Expression<Func<TParam, TExpressionResult1>>, Expression<Func<TParam, TExpressionResult2>>, Expression<Func<TParam, TExpressionResult3>>) expressions,
+        Expression<Func<TExpressionResult1, TExpressionResult2, TExpressionResult3, TProjectionResult>> projector
+        )
+    {
+        var parameter = Expression.Parameter(typeof(TParam), "param");
+
+        var arg1 = Expression.Invoke(expressions.Item1, parameter);
+        var arg2 = Expression.Invoke(expressions.Item2, parameter);
+        var arg3 = Expression.Invoke(expressions.Item3, parameter);
+
+        var projection = Expression.Invoke(projector, arg1, arg2, arg3);
+
+        return Expression.Lambda<Func<TParam, TProjectionResult>>(projection, parameter);
+    }
+
+    /// <summary>
+    ///     Returns a new expression that projects the result of the given expressions.
+    /// </summary>
+    /// <typeparam name="TParam">
+    ///     The type of the parameter of the expressions.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult1">
+    ///     The type of the result of the first expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult2">
+    ///     The type of the result of the second expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult3">
+    ///     The type of the result of the third expression.
+    /// </typeparam>
+    /// <typeparam name="TExpressionResult4">
+    ///     The type of the result of the fourth expression.
+    /// </typeparam>
+    /// <typeparam name="TProjectionResult">
+    ///     The type of the result of the projection expression.
+    /// </typeparam>
+    /// <param name="expressions">
+    ///     The expressions to project the result of.
+    /// </param>
+    /// <param name="projector">
+    ///     The expression to project the result of the expressions.
+    /// </param>
+    /// <returns>
+    ///     The new expression that projects the result of the given expressions.
+    /// </returns>
+    public static Expression<Func<TParam, TProjectionResult>> Project<TParam, TExpressionResult1, TExpressionResult2, TExpressionResult3, TExpressionResult4, TProjectionResult>(
+        this (Expression<Func<TParam, TExpressionResult1>>, Expression<Func<TParam, TExpressionResult2>>, Expression<Func<TParam, TExpressionResult3>>, Expression<Func<TParam, TExpressionResult4>>) expressions,
+        Expression<Func<TExpressionResult1, TExpressionResult2, TExpressionResult3, TExpressionResult4, TProjectionResult>> projector
+        )
+    {
+        var parameter = Expression.Parameter(typeof(TParam), "param");
+
+        var arg1 = Expression.Invoke(expressions.Item1, parameter);
+        var arg2 = Expression.Invoke(expressions.Item2, parameter);
+        var arg3 = Expression.Invoke(expressions.Item3, parameter);
+        var arg4 = Expression.Invoke(expressions.Item4, parameter);
+
+        var projection = Expression.Invoke(projector, arg1, arg2, arg3, arg4);
+
+        return Expression.Lambda<Func<TParam, TProjectionResult>>(projection, parameter);
+    }
+}
+
---
+++ test/Benjitek.Expressions.Tests/ProjectorExpressionsExtensionsTests.cs
@@ -0,0 +1,148 @@
+﻿using System.Linq.Expressions;
+
+namespace Benjitek.Expressions.Tests;
+
+public class ProjectorExpressionsExtensionsTests
+{
+    public class WhenProjectingOneExpression
+    {
+        [Fact]
+        public void ShouldProjectExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression = x => x + 1;
+            Expression<Func<int, int>> projector = x => x * 4;
+
+            // Act
+            var result = expression.Project(projector);
+
+            // Assert
+            Assert.Equal(4, result.Compile()(0));
+        }
+
+        [Fact]
+        public void ShouldReturnNewExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression = x => x + 1;
+            Expression<Func<int, int>> projector = x => x * 2;
+
+            // Act
+            var result = expression.Project(projector);
+
+            // Assert
+            Assert.NotSame(expression, result);
+        }
+    }
+
+    public class WhenProjectingTwoExpressions
+    {
+        [Fact]
+        public void ShouldProjectExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression1 = x => x + 1;
+            Expression<Func<int, int>> expression2 = x => x * 2;
+            Expression<Func<int, int, int>> projector = (x, y) => x + y;
+
+            // Act
+            var result = (expression1, expression2).Project(projector);
+
+            // Assert
+            Assert.Equal(7, result.Compile()(2));
+        }
+
+        [Fact]
+        public void ShouldReturnNewExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression1 = x => x + 1;
+            Expression<Func<int, int>> expression2 = x => x * 2;
+            Expression<Func<int, int, int>> projector = (x, y) => x + y;
+
+            // Act
+            var result = (expression1, expression2).Project(projector);
+
+            // Assert
+            Assert.NotSame(expression1, result);
+            Assert.NotSame(expression2, result);
+        }
+    }
+
+    public class WhenProjectingThreeExpressions
+    {
+        [Fact]
+        public void ShouldProjectExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression1 = x => x + 1;
+            Expression<Func<int, int>> expression2 = x => x * 2;
+            Expression<Func<int, int>> expression3 = x => x * 4;
+            Expression<Func<int, int, int, int>> projector = (x, y, z) => x + y + z;
+
+            // Act
+            var result = (expression1, expression2, expression3).Project(projector);
+
+            // Assert
+            Assert.Equal(15, result.Compile()(2));
+        }
+
+        [Fact]
+        public void ShouldReturnNewExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression1 = x => x + 1;
+            Expression<Func<int, int>> expression2 = x => x * 2;
+            Expression<Func<int, int>> expression3 = x => x - 1;
+            Expression<Func<int, int, int, int>> projector = (x, y, z) => x + y + z;
+
+            // Act
+            var result = (expression1, expression2, expression3).Project(projector);
+
+            // Assert
+            Assert.NotSame(expression1, result);
+            Assert.NotSame(expression2, result);
+            Assert.NotSame(expression3, result);
+        }
+    }
+
+    public class WhenProjectingFourExpressions
+    {
+        [Fact]
+        public void ShouldProjectExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression1 = x => x + 1;
+            Expression<Func<int, int>> expression2 = x => x * 2;
+            Expression<Func<int, int>> expression3 = x => x * 4;
+            Expression<Func<int, int>> expression4 = x => x * 8;
+            Expression<Func<int, int, int, int, int>> projector = (w, x, y, z) => w + x + y + z;
+
+            // Act
+            var result = (expression1, expression2, expression3, expression4).Project(projector);
+
+            // Assert
+            Assert.Equal(31, result.Compile()(2));
+        }
+         
+        [Fact]
+        public void ShouldReturnNewExpression()
+        {
+            // Arrange
+            Expression<Func<int, int>> expression1 = x => x + 1;
+            Expression<Func<int, int>> expression2 = x => x * 2;
+            Expression<Func<int, int>> expression3 = x => x - 1;
+            Expression<Func<int, int>> expression4 = x => x - 2;
+            Expression<Func<int, int, int, int, int>> projector = (w, x, y, z) => w + x + y + z;
+
+            // Act
+            var result = (expression1, expression2, expression3, expression4).Project(projector);
+
+            // Assert
+            Assert.NotSame(expression1, result);
+            Assert.NotSame(expression2, result);
+            Assert.NotSame(expression3, result);
+        }
+    }
+}
+
